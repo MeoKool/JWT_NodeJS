e4db4e138f3b0608444ee45d1d0c93c848cc80b8
MeoKool <truongsyquang1202@gmail.com>
2024-04-10 10:08:59 +0700

    update-login-user

--- BackEnd/Controllers/authControllers.js
+++ BackEnd/Controllers/authControllers.js
@@ -21,6 +21,26 @@ const authControllers = {
       res.status(500).json({ message: error.message });
     }
   },
+  loginUser: async (req, res) => {
+    try {
+      const user = await User.findOne({ username: req.body.username });
+      if (!user) {
+        res.status(400).json("User not found");
+      }
+      const validPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+      if (!validPassword) {
+        res.status(400).json("Wrong password");
+      }
+      if (user && validPassword) {
+        res.status(200).json(user);
+      }
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
 };
 
 module.exports = authControllers;
--- BackEnd/Routes/auth.js
+++ BackEnd/Routes/auth.js
@@ -3,5 +3,5 @@ const authControllers = require("../Controllers/authControllers");
 const router = require("express").Router();
 
 router.post("/register", authControllers.registerUser);
-
+router.post("/login", authControllers.loginUser);
 module.exports = router;
